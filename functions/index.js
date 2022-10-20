d6d372fc74ed473dd8214c8ee96e065ce1d87f81
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-20 13:57:35 -0400

    starting firbase cloud

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+import functions from 'firebase-functions'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });

2409561d45b41b6cd2b7f361651ca212e198633c
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-20 14:01:25 -0400

    starting firbase cloud step1

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
+app.use(cors())
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
